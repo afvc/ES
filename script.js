7adbae59b246368a2657e0082d8a5f238c741e65
seniferraz <inesferra95@gmail.com>
2016-11-22 14:05:23 +0000

    Show/Hide Alerts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("#feed").on("click", showHiddenFeed);
+$(".showFeed").on("click", showHiddenFeed);
 
 function showHiddenFeed() {
     if ($(".feedCol").css("display") == "none") {
@@ -10,6 +10,18 @@ function showHiddenFeed() {
         $(".centerSection").removeClass("col-xs-7");
         $(".centerSection").addClass("col-xs-10");
         $(".section-resized").css("padding-right", "5vw");
-        
+    }
+}
+
+$(".showAlerts").on("click", showAllerts);
+
+function showAllerts() {
+    if ($(".alerts").css("display") == "none") {
+        $(".alerts").css("display", "block");
+        $(".userAllerts").css("color", "red");
+
+    } else {
+        $(".alerts").css("display", "none");
+        $(".userAllerts").css("color", "#171719");
     }
 }
