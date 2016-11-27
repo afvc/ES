135141bf8922c56ac203df3de1416515f0e45a51
Luis Coimbra <lcoimbra2006@gmail.com>
2016-11-27 17:23:11 +0000

    big fixes + database changes

--- app/views.py
+++ app/views.py
@@ -1,26 +1,19 @@
-# import getpass
-# import os
 from pprint import pprint
-from flask import Blueprint, render_template, url_for, g, request
-# import copy
+from flask import Blueprint, render_template, url_for, request, redirect
 import functions
-import sqlite3
 
 views = Blueprint('views', __name__)
-
-DATABASE = 'app/es.db'
 
 privateToken = 'qxzH6TqLBS1kZstjiCMN'
 
 projectID = None
 curP = None
 curB = None
-user = functions.getUserInformation(privateToken)['username']
 
 @views.route("/")
 @views.route("/index.html")
 def index():
-    userInfo = updateInfo()
+    userInfo = functions.updateInfo(privateToken)
 
     projName = 'nnnn'
     branches = None
@@ -34,7 +27,7 @@ def index():
     return render_template("index.html", projectName = projName, projectsList = projects, branchesList = branches, currentUser = userInfo, currentProject = curP, currentBranch = curB)
 
 
-@views.route("/index.html/<page>/<type>/<text>", methods=['GET'])
+@views.route("/<page>.html/<type>/<text>", methods=['GET'])
 def changeProjectOrBranch(page, type, text):
 
     if (type == 'project'):
@@ -57,16 +50,16 @@ def changeProjectOrBranch(page, type, text):
     res = page.split('-')
 
     if (res[0] == 'index'):
-        return index()
+        return redirect('/')
     elif (res[0] == 'members'):
-        return members()
+        return redirect('/members.html')
     elif (res[0] == 'profile'):
-        return profile(res[1])
+        return redirect('/profile-inside.html/' + res[1])
 
 
 @views.route("/members.html")
 def members():
-    userInfo = updateInfo()
+    userInfo = functions.updateInfo(privateToken)
 
     projects = functions.getProjects(privateToken)
     branches = functions.getBranchInformation(privateToken, projectID)
@@ -77,9 +70,9 @@ def members():
     return render_template("members.html", projectName = projName, projectsList = projects, branchesList = branches, currentUser = userInfo, currentProject = curP, currentBranch = curB, membersList = members)
 
 
-@views.route("/profile-inside.html/<username>")
+@views.route("/profile-inside.html/<username>", methods=['GET'])
 def profile(username):
-    userInfo = updateInfo()
+    userInfo = functions.updateInfo(privateToken)
 
     members = functions.getMembersInformation(privateToken, projectID)
     member = dict()
@@ -87,7 +80,7 @@ def profile(username):
         if (member["username"] == username):
             break
 
-    res = query_db('SELECT * FROM perfil WHERE username = ?', [username], one=True)
+    res = functions.query_db('SELECT * FROM profile WHERE username = ?', [username], one=True)
 
     if (res is None):
         check = None
@@ -110,9 +103,10 @@ def profile(username):
 
 @views.route("/profile-edit.html")
 def profileEdit():
-    userInfo = updateInfo()
+    userInfo = functions.updateInfo(privateToken)
 
-    res = query_db('SELECT * FROM perfil WHERE username = ?', [user], one=True)
+    user = functions.getUserInformation(privateToken)['username']
+    res = functions.query_db('SELECT * FROM profile WHERE username = ?', [user], one=True)
 
     info = dict()
     info['skype'] = res[2]
@@ -128,9 +122,9 @@ def profileEdit():
     return render_template("profile-edit.html", projectName = projName, projectsList = projects, branchesList = branches, currentUser = userInfo, currentProject = curP, currentBranch = curB, userInfoDB = info)
 
 
-@views.route("/profile-inside.html/" + user, methods=['POST'])
-def editPOST():
-    userInfo = updateInfo()
+@views.route("/profile-inside.html/<username>", methods=['POST'])
+def editPOST(username):
+    userInfo = functions.updateInfo(privateToken)
 
     if (int(request.form['choice'])):
         skype = request.form['skype']
@@ -138,65 +132,8 @@ def editPOST():
         twitter = request.form['twitter']
         website = request.form['website']
         bio = request.form['bio']
-        query_db('UPDATE perfil SET skype = ?, linkedin = ?, twitter = ?, website = ?, bio = ?, noSync = ? WHERE username = ?;', [skype, linkedin, twitter, website, bio, 1, user], one=True)
+        functions.query_db('UPDATE profile SET skype = ?, linkedin = ?, twitter = ?, website = ?, bio = ?, noSync = ? WHERE username = ?;', [skype, linkedin, twitter, website, bio, 1, username], one=True)
     else:
-        query_db('UPDATE perfil SET noSync = ? WHERE username = ?;', [0, user], one=True)
+        functions.query_db('UPDATE profile SET noSync = ? WHERE username = ?;', [0, username], one=True)
 
-    return profile(user)
-
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-# @views.do_teardown_appcontext
-# def close_connection(exception):
-#    db = getattr(g, '_database', None)
-#    if db is not None:
-#        db.close()
-
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    get_db().commit()
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-
-def updateInfo():
-    res = query_db('SELECT * FROM perfil WHERE username = ?', [user], one=True)
-
-    if (res is None):
-        # print('TRUE: ' + str(res2))
-        userInfo = functions.getUserInformation(privateToken)
-        query_db('INSERT INTO perfil (username, name, skype, linkedin, twitter, website, bio, email, noSync) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);', [userInfo['username'], userInfo['name'], userInfo['skype'], userInfo['linkedin'], userInfo['twitter'], userInfo['website_url'], userInfo['bio'], userInfo['email'], 0], one=True)
-    else:
-        # print('FALSE: ' + str(res2))
-        if (res[8]): # res['noSync']
-            userInfo = dict()
-            userInfo['username'] = res[0]
-            userInfo['nome'] = res[1]
-        else:
-            userInfo = functions.getUserInformation(privateToken)
-            query_db('UPDATE perfil SET name = ?, skype = ?, linkedin = ?, twitter = ?, website = ?, bio = ?, email = ? WHERE username = ?;', [userInfo['name'], userInfo['skype'], userInfo['linkedin'], userInfo['twitter'], userInfo['website_url'], userInfo['bio'], userInfo['email'], userInfo['username']], one=True)
-
-    return userInfo
-
-
-
-#########
-# for user in query_db('select * from users'):
-#    print user['username'], 'has the id', user['user_id']
-
-# user = query_db('select * from users where username = ?',
-#                [the_username], one=True)
-# if user is None:
-#    print 'No such user'
-# else:
-#    print the_username, 'has the id', user['user_id']
-###################
-
-# CREATE TABLE perfil (user varchar(75) primary key, skype varchar(150), linkedin varchar(150), twitter varchar(150), website varchar(150), bio varchar(251), email varchar(150));
+    return redirect('/profile-inside.html/' + username)
