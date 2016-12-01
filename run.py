78dd17094dffb637e84b999d6ac5d73c7f89c387
afvc <anafilipavc@gmail.com>
2016-12-01 19:13:54 +0000

    python

--- run.py
+++ run.py
@@ -2,12 +2,10 @@ from app import app
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-<<<<<<< HEAD
-    
-=======
+
 
 app.secret_key = '\3Zr47jyXLwf/,?KTaF12R~X@H!jmM]'
 
->>>>>>> filipa
+
 if __name__ == "__main__":
     app.run(threaded=True)
