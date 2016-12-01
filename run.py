6c4278872dd5f43ac4f1530e1cd20aca9349741d
seniferraz <inesferra95@gmail.com>
2016-12-01 10:47:24 +0000

    run.py file

--- run.py
+++ run.py
@@ -2,14 +2,8 @@ from app import app
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-<<<<<<< HEAD
-    
-if __name__ == "__main__":
-    app.run()
-=======
 
 app.secret_key = '\3Zr47jyXLwf/,?KTaF12R~X@H!jmM]'
 
 if __name__ == "__main__":
     app.run(threaded=True)
->>>>>>> master
