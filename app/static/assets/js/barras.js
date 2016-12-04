4a8862f8f12a1a4f4894a1d4ec237006206d407b
seniferraz <inesferra95@gmail.com>
2016-12-04 17:15:50 +0000

    feed collapse arrangements and icons

--- app/static/assets/js/barras.js
+++ app/static/assets/js/barras.js
@@ -4,12 +4,12 @@ function showHiddenFeed() {
     if ($(".feedCol").css("display") == "none") {
         $(".feedCol").css("display", "block");
         $(".centerSection").removeClass("col-xs-10");
-        $(".centerSection").addClass("col-xs-7");
+        $(".centerSection").addClass("col-xs-8");
     } else {
         $(".feedCol").css("display", "none");
-        $(".centerSection").removeClass("col-xs-7");
+        $(".centerSection").removeClass("col-xs-8");
         $(".centerSection").addClass("col-xs-10");
-        $(".section-resized").css("padding-right", "5vw");
+//        $(".section-resized").css("padding-right", "5vw");
     }
 }
 
